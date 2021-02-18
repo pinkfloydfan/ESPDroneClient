646c64c5a1aab4deb10dd478181424e909104205
pinkfloydfan <jxchen98@gmail.com>
2021-02-18 17:16:17 +0000

    Cleaned up the video display component

--- src/Components/VideoDisplay.js
+++ src/Components/VideoDisplay.js
@@ -10,12 +10,6 @@ class VideoDisplay extends React.Component {
                 }
             }>
 
-                <div>
-                    aight this is supposed to be an abstraction of the video from the drone, need to decode this!
-                </div>
-                <div>
-                    {this.props.imageString}
-                </div>
                 <img src = {this.props.imageString} 
                      style = {{
                          width: 640, height: 480
